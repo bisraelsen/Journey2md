d4424c4f453eddc9e3b112a3ef779c0f70719695
Brett Israelsen <brett.israelsen@gmail.com>
2015-08-18 20:19:51 -0600

    added mood translation, trying inline photo markup... still need to test it

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import json
 import datetime
 import sys
 import os
+import shutil
 
 in_folder = 'journey-test/'
 files = os.listdir(in_folder)
@@ -20,18 +21,32 @@ for entry in files:
     text = '\n' + str(data['text'])
 
     # TODO: Convert mood to text values
-    mood = '\nmood - ' + str(data['mood'])
+    mood_dict = {0 :'None',
+                 1 :'Stationary',
+                 2 :'Eating',
+                 3 :'Walking',
+                 4 :'Running',
+                 5 :'Biking',
+                 6 :'Automotive',
+                 7 :'Flying'}
+                 
+    mood = '\nmood - ' + mood_dict[data['mood']]
     weather = '\nweather - ' + str(data['weather'])
-    photos = '\nattached photos - ' + str(data['photos'])
+    photos = '\n[attached photos](' + str(data['photos']) +')'
 
     # construct the output list
-    output = [dateText, text, '\n---', mood]
+    if data['photos']:
+        output = [dateText, text, photos, '\n---', mood]
+    else:
+        output = [dateText, text, '\n---', mood]
     if not data['weather']['id'] == -1:
         output.append(weather)
-    if data['photos']:
-        output.append(photos)
 
     for itm in output:
         fname = outFname + '.md'
         with open(os.path.join('output', fname), 'a') as ofile:
             print(itm, file=ofile)
+
+    if data['photos']:
+        photoname = data['photos'][0]
+        shutil.copyfile(photoname,os.path.join('output',photoname))
